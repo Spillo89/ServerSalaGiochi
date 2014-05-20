d404e9afda4116bd9316799e0e98e1f6060d0be7
spillo89 <vezzoni.gianluca@gmail.com>
2014-05-20 16:30:14 +0200

    alcune query

--- ServerSalaGiochi/src/interfacciaDB/StatementDB.java
+++ ServerSalaGiochi/src/interfacciaDB/StatementDB.java
@@ -3,5 +3,33 @@ package interfacciaDB;
 public class StatementDB {
 	
 	// tutte le query del database
-
+	
+	// inserire utente nel database
+	static String instertClient = "INSERT INTO clienti " +
+			"(user, psw , punti) " +
+			"VALUES ( ?, ?, 100, )";
+	
+	// recupero utente dal database
+	static String takeClient = "SELECT * FROM clienti WHERE user = ? AND psw = ?";
+	
+	// giocatore che fa una partita alla slot
+	
+	static String partitaSlot = "UPDATE clienti SET " +
+			"punti = ?, " +
+			"slot_giocata = ?, " +
+			"WHERE user = ? ";
+	
+	
+	// giocatore che fa una partita a rubamazzo
+	
+	static String partitaRb = "UPDATE clienti SET " +
+			"punti = ?, " +
+			"rb_giocata = ?, " +
+			"WHERE user = ? ";
+	
+	// giocatore che vince una partita a rubamazzo
+	static String vincitaRb = "UPDATE clienti SET " +
+			"punti = ?, " +
+			"rb_vinta = ?, " +
+			"WHERE user = ? ";
 }
