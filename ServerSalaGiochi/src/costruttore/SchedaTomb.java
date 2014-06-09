4f417bf8aec13d6a49aa7f358121c6e2116b3b42
spillo89 <vezzoni.gianluca@gmail.com>
2014-06-09 11:38:56 +0200

    tombola

---
+++ ServerSalaGiochi/src/costruttore/SchedaTomb.java
@@ -0,0 +1,27 @@
+package costruttore;
+
+public class SchedaTomb {
+	
+	private static String ValoriScheda[][] = new String[3][9];
+
+	
+	
+	public void risultatoslot(String valorischeda[][]) {
+		
+		this.ValoriScheda=valorischeda;
+	}
+
+
+
+
+	public static String[][] getValoriScheda() {
+		return ValoriScheda;
+	}
+
+
+	public void setValoriScheda(String[][] valoriScheda) {
+		ValoriScheda = valoriScheda;
+	}
+
+
+}
--- ServerSalaGiochi/src/tombola/Tabella.java
+++ ServerSalaGiochi/src/tombola/Tabella.java
@@ -1,6 +1,8 @@
 package tombola;
 
+import java.util.ArrayList;
 import java.util.Random;
+import costruttore.SchedaTomb;
 
 /* oggetto che crea l'istanza tabella */
 
@@ -10,29 +12,37 @@ public class Tabella {
 	/**
 	 * @param args
 	 */
-	public int tabella[][];
+	public String tabella[][];
+	public ArrayList<SchedaTomb> schede= new ArrayList<SchedaTomb>();
 	Random rand = new Random();
-	public Tabella (Tabellone tab){
+	public SchedaTomb Tabella (Tabellone tab){
+		SchedaTomb scheda=null;
 		tab.reset();
 		int estratto = 0;
-		tabella = new int [3][9];
-		for (int decina = 0; decina<9; decina ++)
+		tabella = new String [3][9];
+		// riempio la tabella con tutti i numeri, ordinati per decine
+		for (int decina = 0; decina<9; decina ++){
 			for(int j=0; j<3; j++){
-				tabella[j][decina] = tab.estraiColonna(decina);
+				tabella[j][decina] = String.valueOf(tab.estraiColonna(decina));
 				System.out.println("numero riga :"+j+" colonna: "+decina+": --->"+tabella[j][decina]);
 			}
+		}
+		
+		// vado a cancellare 4 numeri per ogni riga, mettendoli a 0
 			for(int i=0; i<3;i++){
 				for(int j=0; j<4; j++){
 					do{
 						estratto = rand.nextInt(9);
-					}while(tabella[i][estratto] == 0);
-					tabella[i][estratto] = 0;
+					}while(tabella[i][estratto].equalsIgnoreCase("0"));
+					tabella[i][estratto] = "0";
 				}
 			tab.reset();	
 			}
+			scheda.setValoriScheda(tabella);
+			return scheda;
 	}
 	
-	public int prendiNumero (int riga, int colonna){
+	public String prendiNumero (int riga, int colonna){
 		return tabella[riga][colonna];
 	}
 }
