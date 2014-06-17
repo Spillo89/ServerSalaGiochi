e5f373426f66f63dc3b4616326b633f48a381f4e
MangoKid <MangoKid@192.168.2.252>
2014-06-17 15:54:24 +0200

    logica principale

--- ServerSalaGiochi/src/serverdecoder/ServerDecoderLogin.java
+++ ServerSalaGiochi/src/serverdecoder/ServerDecoderLogin.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 import costruttore.Utente;
 public class ServerDecoderLogin {
 	public static void decoderlogin(String stringainiziale){
-
+		
 		StringTokenizer st = new StringTokenizer(stringainiziale, "#");
 		if(st.nextToken().equalsIgnoreCase("LOGIN")){
 			Utente.setNomeUtente(st.nextToken());
--- ServerSalaGiochi/src/socket/SimpleThread.java
+++ ServerSalaGiochi/src/socket/SimpleThread.java
@@ -5,11 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.StringTokenizer;
+
+import costruttore.UtenteLogin;
 
 import rubaMazzo.PartitaRubamazzo;
+import serverdecoder.ServerDecoderLogin;
+import serverdecoder.ServerDecoderRegistrazione;
+import serverencoder.ServerEncoderLogin;
+import serverencoder.ServerEncoderRegistrazione;
 import tombola.PartitaTombola;
 
 public class SimpleThread extends Thread {
+
+	String parolachiave=null;
 
 	public SimpleThread(Socket clientSocket, PartitaTombola partitatombola, PartitaRubamazzo partitarubamazzo) {
 		// TODO Auto-generated constructor stub
@@ -17,29 +26,69 @@ public class SimpleThread extends Thread {
 
 		PrintWriter writer = null; 
 		BufferedReader reader = null;
-		String[] NomePsw=null;
+		String dainviare=null;
+		String stringa=null;
+
 		try {
 			//Inizializzo la scrittura sul socket 
 			writer = new PrintWriter(clientSocket.getOutputStream(), true); 
 			//Inizializzo la lettura dal socket 
 			reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
-			//Leggo una stringa dal socket 
-			String stringa = reader.readLine(); 
-			System.out.println("Ricevuta stringa: "+stringa);
-			if(stringa.equalsIgnoreCase("exit")){
-				try { reader.close(); } catch (IOException e) {e.printStackTrace();} 
-				writer.close(); 
-				if(!clientSocket.isClosed()){ 
-					try {clientSocket.close(); } catch (IOException e) {e.printStackTrace();} 
-				} 
-			}
-			NomePsw = ServerDecoder.dividi(stringa);
-			if(NomePsw[2].equalsIgnoreCase("l")){
+
+
+
+
+
+			do{
+				//Leggo una stringa dal socket 
+				stringa = reader.readLine(); 
+				System.out.println("Ricevuta stringa: "+stringa);
+				if(stringa.equalsIgnoreCase("exit")){
+					try { reader.close(); } catch (IOException e) {e.printStackTrace();} 
+					writer.close(); 
+					if(!clientSocket.isClosed()){ 
+						try {clientSocket.close(); } catch (IOException e) {e.printStackTrace();} 
+					} 
+				}
+				StringTokenizer st = new StringTokenizer(stringa, "#");
+				switch(st.nextToken()){
+					case "LOGIN": 
+						ServerDecoderLogin.decoderlogin(stringa);
+					case "REGISTRAZIONE": 
+						ServerDecoderRegistrazione.decoderregistrazione(stringa);				
+				}
+
+				//controllare in caso di login se l'utente esiste nel db(se si imposto parolachiave="LOGINOK" in caso contrario imposto parolachiave="LOGINKO"), controllare in caso di registrazione se il nome utente è già presente nel db e se sono presenti tuti i dati richiesti se no aggiungere tutto(in caso che sia tutto giusto parolachiave="REGISTRAZIONEOK" altrimenti parolachiave="REGISTRAZIONEKO")
+
+				switch(parolachiave){
+					case"LOGINOK": 
+						UtenteLogin utentelogin=null;
+						//salvo i dati che mi servono in utentelogin
+						dainviare=ServerEncoderLogin.login(utentelogin);
+						writer.write(dainviare);
+						writer.flush(); 
+
+					case"LOGINKO":
+						dainviare="KO#le credenziali inserite non sono corrette\n";
+						writer.write(dainviare);
+						writer.flush(); 
+					case"REGISTRAZIONEOK":
+						UtenteLogin utenteregistrazione=null;
+						//salvo i dari che mi servono in utetnelogin
+						dainviare=ServerEncoderRegistrazione.registrazione(utenteregistrazione);
+						writer.write(dainviare);
+						writer.flush(); 
+					case"REGISTRAZIONEKO":
+						dainviare="KO#il nomeutente è già presente o le credenziali non sono complete\n";
+						writer.write(dainviare);
+						writer.flush(); 
+				}
 				
-			}
-			//Scrivo una stringa sul socket 
-			writer.write(stringa); 
-			writer.flush(); 
+			}while(parolachiave.equalsIgnoreCase("LOGINKO")||parolachiave.equalsIgnoreCase("REGISTRAZIONEKO"));
+
+			
+			
+			
 		} catch (IOException e) { 
 			System.err.println("Errore di comunicazione."); 
 		} finally { //Alla fine chiudo sempre il reader, il writer, il socket e il serverSocket 
